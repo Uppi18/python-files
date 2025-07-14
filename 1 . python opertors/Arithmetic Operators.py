93aa9db14c39b24e3a79d0e4e0651c42aaf9a048
Uppi18 <derangulaupendra744@gmail.com>
2025-07-14 14:15:03 +0530

    added new file for Arithmetic Operators

--- 1 . python opertors/Arithmetic Operators.py
+++ 1 . python opertors/Arithmetic Operators.py
@@ -1,5 +1,20 @@
-a=400/2
-b=400
-print(a+b)
-print(a-b)
-print(a*b)
+
+print(a+b)   #Addition
+print(a-b)   #subtraction
+print(a*b)   #Multiplication
+print(a/b)   #division
+print(a//b)  #floor division
+print(a%b)   #Modulus (Remainder)
+print(a**b)  #Exponentiation
+
+
+# a=400/2 , a =200
+# b=400
+
+a + b = 600.0
+a - b = -200.0
+a * b = 80000.0
+a / b = 0.5
+a // b = 0.0
+a % b = 200.0
+a ** b = very large number (1.6069380442589903e+46)
